d03c71560947f601a9180742b06ddbbc0d5afed8
Frey Hargarten <harga012@umn.edu>
2018-11-20 12:22:09 -0600

    updates

--- app/index.js
+++ app/index.js
@@ -92,7 +92,7 @@ function chartIncome() {
             // xFormat: '%Y-%m-%d %H:%M:%S',
             columns: [
                 ['x', 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018],
-                ['Rate', 67807,75638,71416,72898,68932,71287,66630,66914,67207,61228,62745,57597,61686,64598,66279,68172,69599,70218, null, null]
+                ['Rate', 69422,77440,73117,74635,70574,72986,68218,68508,68808,62687,64240,58951,63156,66098,67798,69696,71114,71728, 71920, null]
             ],
             type: 'line',
             labels: {
@@ -106,7 +106,7 @@ function chartIncome() {
         },
         point: {
             show: true,
-            r: function(d) { if (d.x == 2016) { return 6;} else { return 2.5; } }
+            r: function(d) { if (d.x == 2017) { return 6;} else { return 2.5; } }
         },
         color: {
             pattern: ['#333333']
@@ -169,101 +169,6 @@ function chartIncome() {
 
 chartIncome();
 
-
-//unemployment chart
-function chartUnemployment() {
-    var padding = {
-        top: 0,
-        right: 40,
-        bottom: 20,
-        left: 60,
-    };
-
-    var chartTrend = c3.generate({
-        bindto: "#chartUnemployment",
-        padding: padding,
-        data: {
-            x: 'x',
-            // xFormat: '%Y-%m-%d %H:%M:%S',
-            columns: [
-                ['x', 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018],
-                ['Rate', 2.5,3,3.4,4.5,4.5,4.9,4.3,4.1,4.3,4.7,7,7.7,6.9,5.8,5.3,4.6,3.8,3.7, null, null]
-            ],
-            type: 'line',
-            labels: {
-                format: {
-                    // 'Rate': d3.format(',.1')
-                }
-            }
-        },
-        legend: {
-            show: false
-        },
-        point: {
-            show: true,
-            r: function(d) { if (d.x == 2016) { return 6;} else { return 2.5; } }
-        },
-        color: {
-            pattern: ['#333333']
-        },
-        axis: {
-            // rotated: true,
-            y: {
-                max: 10,
-                min: 0,
-                padding: {
-                    bottom: 0,
-                    top: 0
-                },
-                tick: {
-                    count: 6,
-                    values: [0, 2, 4, 6, 8, 10],
-                    format: d3.format('.1f')
-                }
-            },
-            x: {
-                // type: 'timeseries',
-                padding: {
-                    right: 0,
-                    left: 0
-                },
-                tick: {
-                    count: 4,
-                    values: [1999, 2003, 2011, 2018],
-                    multiline: false,
-                }
-            }
-        },
-         regions: [
-          {axis: 'x', start: 1999, end: 2003, class: 'ind'},
-          {axis: 'x', start: 2003, end: 2011, class: 'gop'},
-          {axis: 'x', start: 2011, end: 2018, class: 'dfl'},
-        ],
-      tooltip: {
-        contents: function(d, defaultTitleFormat, defaultValueFormat, color) {
-          return '<div class="chart-tooltip">' +
-            '<span class="tooltip-label">' + d[0].x + ':</span>' +
-            '<span class="tooltip-value">' + defaultValueFormat(d[0].value) + '</span>' +
-            '</div>';
-        }
-      },
-        grid: {
-            focus:{
-                show:false
-            },
-        x: {
-            lines: [
-                {value: '2007', text: 'Great Recession starts', position: 'start', class: 'grayline'},
-                {value: '2009', text: 'Great Recession ends', position: 'start', class: 'grayline'}
-            ]
-        }
-     }
-    });
-}
-
-chartUnemployment();
-
-
 //unemployment chart
 function chartHousing() {
     var padding = {
@@ -281,7 +186,7 @@ function chartHousing() {
             // xFormat: '%Y-%m-%d %H:%M:%S',
             columns: [
                 ['x', 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018],
-                ['Rate', 0.22,0.22,0.26,0.26,0.27,0.29,0.31,0.33,0.33,0.34,0.33,0.33,0.33,0.30,0.28,0.28,0.27, 0.27, null, null]
+                ['Rate', 0.22,0.22,0.26,0.26,0.27,0.29,0.31,0.33,0.33,0.34,0.33,0.33,0.33,0.30,0.28,0.28,0.27,0.26,0.24,null]
             ],
             type: 'line',
             labels: {
@@ -295,7 +200,7 @@ function chartHousing() {
         },
         point: {
             show: true,
-            r: function(d) { if (d.x == 2016) { return 6;} else { return 2.5; } }
+            r: function(d) { if (d.x == 2017) { return 6;} else { return 2.5; } }
         },
         color: {
             pattern: ['#333333']
@@ -357,6 +262,98 @@ function chartHousing() {
 
 chartHousing();
 
+//unemployment chart
+function chartUnemployment() {
+    var padding = {
+        top: 0,
+        right: 40,
+        bottom: 20,
+        left: 60,
+    };
+
+    var chartTrend = c3.generate({
+        bindto: "#chartUnemployment",
+        padding: padding,
+        data: {
+            x: 'x',
+            // xFormat: '%Y-%m-%d %H:%M:%S',
+            columns: [
+                ['x', 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018],
+                ['Rate', 2.9,3.4,4.4,4.4,4.9,4.3,4.1,4.3,4.7,6.7,7.7,7.1,5.9,5.4,4.7,3.8,3.7,3.9,3.3,null]
+            ],
+            type: 'line',
+            labels: {
+                format: {
+                    // 'Rate': d3.format(',.1')
+                }
+            }
+        },
+        legend: {
+            show: false
+        },
+        point: {
+            show: true,
+            r: function(d) { if (d.x == 2017) { return 6;} else { return 2.5; } }
+        },
+        color: {
+            pattern: ['#333333']
+        },
+        axis: {
+            // rotated: true,
+            y: {
+                max: 10,
+                min: 0,
+                padding: {
+                    bottom: 0,
+                    top: 0
+                },
+                tick: {
+                    count: 6,
+                    values: [0, 2, 4, 6, 8, 10],
+                    format: d3.format('.1f')
+                }
+            },
+            x: {
+                // type: 'timeseries',
+                padding: {
+                    right: 0,
+                    left: 0
+                },
+                tick: {
+                    count: 4,
+                    values: [1999, 2003, 2011, 2018],
+                    multiline: false,
+                }
+            }
+        },
+         regions: [
+          {axis: 'x', start: 1999, end: 2003, class: 'ind'},
+          {axis: 'x', start: 2003, end: 2011, class: 'gop'},
+          {axis: 'x', start: 2011, end: 2018, class: 'dfl'},
+        ],
+      tooltip: {
+        contents: function(d, defaultTitleFormat, defaultValueFormat, color) {
+          return '<div class="chart-tooltip">' +
+            '<span class="tooltip-label">' + d[0].x + ':</span>' +
+            '<span class="tooltip-value">' + defaultValueFormat(d[0].value) + '</span>' +
+            '</div>';
+        }
+      },
+        grid: {
+            focus:{
+                show:false
+            },
+        x: {
+            lines: [
+                {value: '2007', text: 'Great Recession starts', position: 'start', class: 'grayline'},
+                {value: '2009', text: 'Great Recession ends', position: 'start', class: 'grayline'}
+            ]
+        }
+     }
+    });
+}
+
+chartUnemployment();
 
 //jobs chart chart
 function chartJobs() {
@@ -375,7 +372,7 @@ function chartJobs() {
             // xFormat: '%Y-%m-%d %H:%M:%S',
             columns: [
                 ['x', 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018],
-                ['Rate', 2621400, 2683100, 2687800, 2662900, 2658400, 2678900, 2720900, 2755900, 2768900, 2760400, 2652000, 2638000, 2685000, 2727400, 2775100, 2813500, 2855900, 2895600, null, null]
+                ['Rate', 2621400, 2683100, 2687800, 2662900, 2658400, 2678900, 2720900, 2755900, 2768900, 2760400, 2652000, 2638000, 2685000, 2727400, 2775100, 2813500, 2855900, 2895600, 2932083, null]
             ],
             type: 'line',
             labels: {
@@ -389,7 +386,7 @@ function chartJobs() {
         },
         point: {
             show: true,
-            r: function(d) { if (d.x == 2016) { return 6;} else { return 2.5; } }
+            r: function(d) { if (d.x == 2017) { return 6;} else { return 2.5; } }
         },
         color: {
             pattern: ['#333333']
