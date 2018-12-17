ec6ab675f08787a4540ddbdc55b75c62285ff38f
Frey Hargarten <harga012@umn.edu>
2018-12-17 15:55:38 -0600

    updates

--- app/index.js
+++ app/index.js
@@ -82,7 +82,7 @@ if (selected == "all") {
 
 
 function buildGrid(category,parent,target,label,colorClass,symbol,change,xEnd,yLimit,yFormat,yTicks,linecolor1,linecolor2,linecolor3,data) {
-    $(parent).append('<div class="smallchart xwidth"><div class="label legendary" subject="' + target + '" style="background-color:' + colorScale(change) + ';">' + label + '<div class="stat">' + symbol + '' + d3.format("+.0%")(change) + '</div></div><div class="chart" id="' + target + '"></div></div>');
+    $(parent).append('<div class="smallchart xwidth ' + category + '"><div class="label legendary" subject="' + target + '" style="background-color:' + colorScale(change) + ';">' + label + '<div class="stat">' + symbol + '' + d3.format("+.0%")(change) + '</div></div><div class="chart" id="' + target + '"></div></div>');
 }
 
 function bigChart(category,parent,target,label,colorClass,symbol,change,xEnd,yLimit,yFormat,yTicks,linecolor1,linecolor2,linecolor3,data) {
@@ -260,6 +260,9 @@ function loadBigChart(target) {
     for (var i=0; i < data.length; i++) {
         if (target == data[i].target) {
             bigChart(data[i].category,data[i].parent,data[i].target,data[i].label,data[i].colorClass,data[i].symbol,data[i].change,data[i].xEnd,data[i].yLimit,data[i].yFormat,data[i].yTicks,data[i].linecolor1,data[i].linecolor2,data[i].linecolor3,['Rate', data[i].data1999,data[i].data2000,data[i].data2001,data[i].data2002,data[i].data2003,data[i].data2004,data[i].data2005,data[i].data2006,data[i].data2007,data[i].data2008,data[i].data2009,data[i].data2010,data[i].data2011,data[i].data2012,data[i].data2013,data[i].data2014,data[i].data2015,data[i].data2016,data[i].data2017,data[i].data2018],data[i].index);
+            $("#loadTitle").html(data[i].title);
+            $("#loadChatter").html(data[i].chatter);
+            $("#loadSource").html(data[i].source);
             break;
         }
     }
@@ -279,3 +282,20 @@ $('#return').on('click', function() {
         scrollTop: $("#topper").offset().top
     }, 1500);
 });
+
+//dropdown filter
+$('.dropItem').on('click', function() {
+   $(".smallchart").hide();
+   $("." + $(this).attr("subject")).show();
+   $("#menu").slideToggle();
+   $("#selectedThing").html("▶ " + $(this).text());
+});
+
+$("#selectedThing").on("click", function() {
+    $("#menu").slideToggle();
+    
+});
+
+$('#all').on('click', function() {
+    $(".smallchart").show();
+ });
